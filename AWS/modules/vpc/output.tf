60873c355afa8d96b39114446fae76ed5184b16b
bhavya-r806 <bhavyar806@gmail.com>
2026-02-19 15:16:10 +0530

    added changes to output.tf

--- AWS/modules/vpc/output.tf
+++ AWS/modules/vpc/output.tf
@@ -1,8 +1,14 @@
-output "vpc_id" {
-  value = aws_vpc.network.id
+output "public_subnet_ids" {
+  value = [
+    for k, v in aws_subnet.subnets : v.id
+    if contains(keys(local.public_subnets), k)
+  ]
 }
 
-output "subnet_ids" {
-  value = aws_subnet.subnets[*].id
+output "private_subnet_ids" {
+  value = [
+    for k, v in aws_subnet.subnets : v.id
+    if contains(keys(local.private_subnets), k)
+  ]
 }
 
