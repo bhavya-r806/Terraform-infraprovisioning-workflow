5b27e56bfef1f9d79ebd431ad46f1f7355293cd9
bhavya-r806 <bhavyar806@gmail.com>
2026-02-19 13:44:40 +0530

    made changes in aws main.tf

--- AWS/modules/vpc/main.tf
+++ AWS/modules/vpc/main.tf
@@ -1,38 +1,35 @@
+# Create VPC
 resource "aws_vpc" "network" {
-  cidr_block = var.vpc_cidr
-
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = merge(
-    var.tags,
-    { Name = var.vpc_name }
-  )
+  tags = merge(var.tags, { Name = var.vpc_name })
 }
 
+# Get available AZs
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# Create subnets
 resource "aws_subnet" "subnets" {
-  for_each = local.all_subnets
+  for_each = toset(range(local.total_subnets)) # iterate over subnet indexes
 
   vpc_id     = aws_vpc.network.id
-  cidr_block = each.value
+  cidr_block = local.all_subnets[each.value]
 
   # Stable AZ assignment
   availability_zone = element(
     data.aws_availability_zones.available.names,
-    index(sort(keys(local.all_subnets)), each.key)
-    % length(data.aws_availability_zones.available.names)
+    each.value % length(data.aws_availability_zones.available.names)
   )
 
   tags = merge(
     var.tags,
     {
-      Name = "${var.vpc_name}-${each.key}"
-      Tier = contains(keys(var.public_subnets), each.key) ? "public" : "private"
+      Name = "subnet-${each.value + 1}"
+      Tier = each.value < var.public_subnets ? "public" : "private"
     }
   )
 }
-
