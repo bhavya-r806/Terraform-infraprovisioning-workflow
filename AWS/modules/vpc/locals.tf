83a81536e86fc264f4f89a33160174438a80fb72
bhavya-r806 <bhavyar806@gmail.com>
2026-02-27 14:39:45 +0530

    edited locals.tf

--- AWS/modules/vpc/locals.tf
+++ AWS/modules/vpc/locals.tf
@@ -1,17 +1,20 @@
 locals {
   public_subnets = {
-    for i in range(var.public_subnets) :
+    for i in range(var.public_subnet_count) :
     "public-${i + 1}" => cidrsubnet(var.vpc_cidr, var.subnet_newbits, i)
   }
 
   private_subnets = {
-    for i in range(var.private_subnets) :
+    for i in range(var.private_subnet_count) :
     "private-${i + 1}" => cidrsubnet(
       var.vpc_cidr,
       var.subnet_newbits,
-      i + var.public_subnets
+      i + var.public_subnet_count
     )
   }
 
-  all_subnets = merge(local.public_subnets, local.private_subnets)
+  all_subnets = merge(
+    local.public_subnets,
+    local.private_subnets
+  )
 }
