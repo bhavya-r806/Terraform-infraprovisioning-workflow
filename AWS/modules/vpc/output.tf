341086a6074c7f9bb3f7239d39ac336c905db57d
bhavya-r806 <bhavyar806@gmail.com>
2026-02-27 14:49:40 +0530

    modified syntax errors in output.tf

--- AWS/modules/vpc/output.tf
+++ AWS/modules/vpc/output.tf
@@ -1,14 +1,14 @@
 output "public_subnet_ids" {
   value = [
     for k, v in aws_subnet.subnets : v.id
-    if contains(keys(local.public_subnet_count), k)
+    if contains(keys(local.public_subnets), k)
   ]
 }
 
 output "private_subnet_ids" {
   value = [
     for k, v in aws_subnet.subnets : v.id
-    if contains(keys(local.private_subnet_count), k)
+    if contains(keys(local.private_subnets), k)
   ]
 }
 
